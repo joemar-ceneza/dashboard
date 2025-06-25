ede75ab6acb41a95ac9f2658e50a899b5d0802bd
Joemar Ceneza <joemar.ceneza@gmail.com>
2025-06-26 02:10:18 +0800

    feat: add homepage with login button linking to /login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <Link href={"/login"} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition">
+        <button>Login</button>
+      </Link>
+    </main>
+  );
 }
