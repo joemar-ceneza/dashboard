8eb2ef824a03c059c0e34c9261531fb2c43c4b77
Joemar Ceneza <joemar.ceneza@gmail.com>
2025-06-26 17:15:58 +0800

    fix(home): wrap login and register links in a div for better structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,14 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen">
-      <Link href={"/login"} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition">
-        <button>Login</button>
-      </Link>
+      <div className="">
+        <Link href={"/login"} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition mx-2">
+          <button>Login</button>
+        </Link>
+        <Link href={"/register"} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition mx-2">
+          <button>Register</button>
+        </Link>
+      </div>
     </main>
   );
 }
